a19ea24609bd1bf4a684e8cf8a8527b893ed69fb
BhavanaRamyaBanala <ramyabanala12@gmail.com>
2024-02-24 14:19:35 +0530

    First Commit

---
+++ Controllers/PizzasController.cs
@@ -0,0 +1,23 @@
+﻿using AutomateJenkinsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutomateJenkinsApp.Controllers
+{
+    public class PizzasController : Controller
+    {
+        static List<Pizza> pizzas = new List<Pizza>() { 
+            new Pizza { Id = 1,Name="Paneer pizza",Price=650},
+            new Pizza { Id = 2,Name="Mushroom pizza",Price=550},
+            new Pizza { Id = 3,Name="Chicken pizza",Price=850},
+        };
+        // GET: Pizzas
+        public ActionResult Index()
+        {
+            return View(pizzas);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace AutomateJenkinsApp.Models
+{
+    [Table("Pizza")]
+    public class Pizza
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price {  get; set; }
+    }
+}
